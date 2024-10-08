fcdd848dcd14762e91c5cfc3e60bfa8e1da8c335
Harshit Verma <Harshit2003v@gmail.com>
2024-10-08 15:41:03 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Anime & Movie API
+
+## Description
+
+The Anime & Movie API is a RESTful API that provides access to trending, popular, and upcoming anime and movies. This API leverages the Jikan API for anime data and The Movie Database (TMDB) API for movie data. It offers endpoints to fetch detailed information about anime and movies, allowing developers to integrate anime and movie functionalities into their applications seamlessly.
+
+## Features
+
+### Anime Endpoints
+
+- `GET` Trending Anime: Fetch the currently trending anime series.
+- `GET` Popular Anime: Retrieve a list of popular anime series.
+- `GET` Upcoming Anime: Get information about upcoming anime releases.
+
+### Movie Endpoints
+
+- `GET` Trending Movies: Fetch the currently trending movies.
+- `GET` Popular Movies: Retrieve a list of popular movies.
+- `GET` Upcoming Movies: Get information about upcoming movie releases.
+
+## Installation
+
+> Yet to figure out myself.
+
+---
---
+++ api.js
@@ -0,0 +1,248 @@
+import express from "express";
+import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express(); 
+const PORT = process.env.PORT || 5033;
+
+const imageUrl = "https://image.tmdb.org/t/p/w500";
+const baseJikanUrl = "https://api.jikan.moe/v4";
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`
+    }
+};
+
+/* =============================================== */
+/*                  BASE ROUTE                     */
+/* =============================================== */
+/* *********************************************** */
+app.get("/", (request, response) => {
+    response.send("Welcome to TMDB + Jikan API 😎🫘");
+});
+
+/* =============================================== */
+/*                  TMDB API ROUTES                */
+/* =============================================== */
+/* *********************************************** */
+
+/* =============================================== */
+/*                  Trending movies                */
+/* =============================================== */
+app.get("/trending-movies", async (request, response) => {
+    try {
+        const url = "https://api.themoviedb.org/3/trending/movie/week?language=en-US";
+
+        const trending = await axios.get(url, options);
+        const trendingData = trending.data.results;
+
+        const trendingMovieArray = trendingData.slice(0, 20).map(movie => ({
+            id: movie.id,
+            title: movie.title,
+            backdrop_path: imageUrl + movie.backdrop_path,
+            overview: movie.overview
+        }));
+
+        response.send(trendingMovieArray);
+    } catch (err) {
+        console.error("Error fetching trending movies:", err);
+        response.status(500).send("Error fetching trending movies.");
+    }
+});
+
+/* =============================================== */
+/*                  Trending TV                    */
+/* =============================================== */
+app.get("/trending-tv", async (request, response) => {
+    try {
+        const url = "https://api.themoviedb.org/3/trending/tv/week?language=en-US";
+
+        const trending = await axios.get(url, options);
+        const trendingData = trending.data.results;
+
+        const trendingTVArray = trendingData.slice(0, 20).map(movie => ({
+            id: movie.id,
+            title: movie.name,
+            backdrop_path: imageUrl + movie.backdrop_path,
+            overview: movie.overview
+        }));
+
+        response.send(trendingTVArray);
+    } catch (err) {
+        console.error("Error fetching trending tv:", err);
+        response.status(500).send("Error fetching trending tv.");
+    }
+});
+
+
+/* =============================================== */
+/*                  Popular TV                     */
+/* =============================================== */
+app.get("/popular-tv", async (request, response) => {
+    try {
+        const url = "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1";
+
+        const popular = await axios.get(url, options);
+        const popularData = popular.data.results;
+
+        const popularTVArray = popularData.slice(0, 20).map(movie => ({
+            id: movie.id,
+            title: movie.name,
+            backdrop_path: imageUrl + movie.backdrop_path,
+            overview: movie.overview
+        }));
+
+        response.send(popularTVArray);
+    } catch (err) {
+        console.error("Error fetching trending tv:", err);
+        response.status(500).send("Error fetching trending tv.");
+    }
+});
+
+/* =============================================== */
+/*                  Popular movies                 */
+/* =============================================== */
+app.get("/popular-movies", async (request, response) => {
+    try {
+        const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+        const popular = await axios.get(url, options);
+        const popularData = popular.data.results;
+
+        const popularMovieArray = popularData.slice(0, 20).map(movie => ({
+            id: movie.id,
+            title: movie.title,
+            backdrop_path: imageUrl + movie.backdrop_path,
+            overview: movie.overview
+        }));
+
+        response.send(popularMovieArray);
+    } catch (err) {
+        console.error("Error fetching popular movies:", err);
+        response.status(500).send("Error fetching popular movies.");
+    }
+});
+
+/* =============================================== */
+/*                  Upcoming movies                */
+/* =============================================== */
+app.get("/upcoming-movies", async (request, response) => {
+    try {
+        const url = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+
+        const upcoming = await axios.get(url, options);
+        const upcomingData = upcoming.data.results;
+
+        const upcomingMovieArray = upcomingData.slice(0, 20).map(movie => ({
+            id: movie.id,
+            title: movie.title,
+            backdrop_path: imageUrl + movie.backdrop_path,
+            overview: movie.overview
+        }));
+
+        response.send(upcomingMovieArray);
+    } catch (err) {
+        console.error("Error fetching upcoming movies:", err);
+        response.status(500).send("Error fetching upcoming movies.");
+    }
+});
+
+app.get("/search-movie", async (request, response) => {
+    const query = request.query.q; // Get the search query from the query parameters
+    console.log(`Searching for movie: ${query}`);
+    // Replace with the actual TMDB API call
+    response.send(`Search results for movie: ${query}`);
+});
+
+app.get("/search-tv", async (request, response) => {
+    const query = request.query.q; // Get the search query from the query parameters
+    console.log(`Searching for TV show: ${query}`);
+    // Replace with the actual TMDB API call
+    response.send(`Search results for TV show: ${query}`);
+});
+
+/* =============================================== */
+/*                  JIKAN API ROUTES               */
+/* =============================================== */
+/* *********************************************** */
+
+/* =============================================== */
+/*                  Trending anime                 */
+/* =============================================== */
+app.get("/trending-anime", async (request, response) => {
+    try {
+        const trendingAnimeUrl = `${baseJikanUrl}/seasons/now`;
+        const trending = await axios.get(trendingAnimeUrl);
+        const trendingAnimeData = trending.data.data;
+
+        const trendingAnimeArray = trendingAnimeData.slice(0, 20).map(anime => ({
+            id: anime.mal_id,
+            title: anime.titles[0].title,
+            image: anime.images.jpg.large_image_url
+        }));
+
+        response.send(trendingAnimeArray);
+    } catch (err) {
+        console.error("Error fetching trending anime:", err);
+        response.status(500).send("Error fetching trending anime.");
+    }
+});
+
+/* =============================================== */
+/*                  Popular anime                  */
+/* =============================================== */
+app.get("/popular-anime", async (request, response) => {
+    try {
+        const popularAnimeUrl = `${baseJikanUrl}/top/anime`;
+        const popular = await axios.get(popularAnimeUrl);
+        const popularAnimeData = popular.data.data;
+
+        const popularAnimeArray = popularAnimeData.slice(0, 20).map(anime => ({
+            id: anime.mal_id,
+            title: anime.titles[0].title,
+            image: anime.images.jpg.large_image_url
+        }));
+
+        response.send(popularAnimeArray);
+    } catch (err) {
+        console.error("Error fetching popular anime:", err);
+        response.status(500).send("Error fetching popular anime.");
+    }
+});
+
+/* =============================================== */
+/*                  Upcoming anime                 */
+/* =============================================== */
+app.get("/upcoming-anime", async (request, response) => {
+    try {
+        const upcomingAnimeUrl = `${baseJikanUrl}/seasons/upcoming`;
+        const upcoming = await axios.get(upcomingAnimeUrl);
+        const upcomingAnimeData = upcoming.data.data;
+
+        const upcomingAnimeArray = upcomingAnimeData.slice(0, 20).map(anime => ({
+            id: anime.mal_id,
+            title: anime.titles[0].title,
+            image: anime.images.jpg.large_image_url
+        }));
+
+        response.send(upcomingAnimeArray);
+    } catch (err) {
+        console.error("Error fetching popular anime:", err);
+        response.status(500).send("Error fetching popular anime.");
+    }
+});
+
+app.get("/search-anime", async (request, response) => {
+    const query = request.query.q; // Get the search query from the query parameters
+    console.log(`Searching for anime: ${query}`);
+    response.send(`Search results for anime: ${query}`);
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
