fe6c3630a971922996578d2403e917ace7591bf8
Harshit Verma <Harshit2003v@gmail.com>
2024-10-16 23:30:27 +0530

    Add redisClient.js for redis caching implementation

---
+++ caching/redisClient.js
@@ -0,0 +1,21 @@
+/*
+ ██████╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ 
+██╔════╝██╔══██╗██╔════╝██║  ██║██║████╗  ██║██╔════╝ 
+██║     ███████║██║     ███████║██║██╔██╗ ██║██║  ███╗
+██║     ██╔══██║██║     ██╔══██║██║██║╚██╗██║██║   ██║
+╚██████╗██║  ██║╚██████╗██║  ██║██║██║ ╚████║╚██████╔╝
+ ╚═════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ 
+*/
+
+import Redis from "ioredis";
+
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+});
+
+redisClient.on('error', (err) => {
+    console.error('Redis error: ', err);
+});
+
+export default redisClient;
