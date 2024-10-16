5cf753c087c9b74cfe12d1a684c89109644f4687
Harshit Verma <Harshit2003v@gmail.com>
2024-10-16 23:55:59 +0530

    Fix deployment for redis

--- caching/redisClient.js
+++ caching/redisClient.js
@@ -12,6 +12,7 @@ import Redis from "ioredis";
 const redisClient = new Redis({
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD
 });
 
 redisClient.on('error', (err) => {
